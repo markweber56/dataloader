cff0fa108aca653c8ceb2922b0d99e60b47fb20a
mark weber <markweber56@gmail.com>
2023-05-24 23:30:03 -0500

    load sample file to DF from data dir

--- src/main/scala/DataLoader.scala
+++ src/main/scala/DataLoader.scala
@@ -1,12 +1,14 @@
 package main.scala.dataloader
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkContext
+import java.io.File
 import org.apache.spark.sql.types._
 
 object DataLoader {
 
   def main(args: Array[String]): Unit = {
+
+    val dataDir: String = "data//price"
 
     val spark = SparkSession
       .builder
@@ -14,7 +16,42 @@ object DataLoader {
       .appName("dataloader")
       .getOrCreate()
 
-    println("starting dataloaders")
+    println("starting dataloader")
+
+    val fileNames: Array[String] = readFilesFromDirectory(dataDir)
+    println(fileNames(0))
+
+    val sampleFile = fileNames(0)
+
+    val schema = StructType(Array(StructField("InstrumentKey", IntegerType, false),
+      StructField("TradeDt", StringType, false),
+      StructField("OpenPriceAmt", FloatType, false),
+      StructField("ClosingAskPriceAmt", FloatType, false),
+      StructField("ClosingBidPriceAmt", FloatType, false),
+      StructField("ClosingBidPriceAmt2", FloatType, false),
+      StructField("ClosingPriceAmt", FloatType, false),
+      StructField("HighestTradingPriceAmt", FloatType, false),
+      StructField("LowestPriceAmt", FloatType, false),
+      StructField("NumberOfTradesQty", IntegerType, false)))
+
+    val priceDF = spark.read.schema(schema).csv(sampleFile)
+
+    priceDF.show(10)
+
+
+  }
+
+  def readFilesFromDirectory(directoryPath: String): Array[String] = {
+    val directory = new File(directoryPath)
+
+    if (directory.exists && directory.isDirectory) {
+      println("directory exists")
+      val files = directory.listFiles.filter(_.isFile)
+      files.map(f => f.toString())
+    } else {
+      print("directory does not exist")
+      Array.empty[String]
+    }
   }
 
 }
